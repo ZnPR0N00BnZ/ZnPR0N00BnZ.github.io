510a0da26cc6a0874b46267cb0701de241b4da90
ZnPR0N00BnZ <limzhineng@hotmail.com>
2024-12-14 20:58:18 -0800

    Olive project done

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const sections = document.querySelectorAll("section");
+// document.addEventListener("DOMContentLoaded", () => {
+//   const sections = document.querySelectorAll("section");
 
-  // Loop through each section
-  sections.forEach((section) => {
-    // Generate a random color
-    const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+//   // Loop through each section
+//   sections.forEach((section) => {
+//     // Generate a random color
+//     const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 
-    // Apply the random color as the background
-    section.style.backgroundColor = randomColor;
-  });
-});
+//     // Apply the random color as the background
+//     section.style.backgroundColor = randomColor;
+//   });
+// });
